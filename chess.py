02db6ceae27118811f82b426c5ff3e84aeba3611
Vincent <generation2geek@gmail.com>
2021-12-19 22:40:01 +0100

    Major improvements (UI, pawn gestion, comments...)

--- chess.py
+++ chess.py
@@ -3,10 +3,28 @@
 import os
 import time
 from random import randint
+import pygame  
+
 
 clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
-pieces = ["r","n","b","q","k","b","n","r"]
+"""
+PIECES :
+p : pawn
+r : rook (tour)
+n : knight (cavalier)
+b : bishop (fou)
+q : queen (dame)
+k : king (roi)
+
+En minuscule : pièce noire
+En majuscule : pièce blanche
+
+Ce système de notation permet d'utiliser le FEN pour charger n'importe quelle position dans le programme (voir initialise_game_board_from_fen)
+Plus d'informations : https://www.chess.com/terms/fen-chess
+"""
+
+
 
 clearConsole()
 
@@ -40,6 +58,7 @@ def initialise_game_board():
 """
 
 def initialise_game_board():
+    pieces = ["r","n","b","q","k","b","n","r"]
     board = create_game_board()
     for i in range(8):
         board[1][i] = "p"
@@ -111,16 +130,16 @@ def is_in_matrix(matrix, x, y):
     """
     return len(matrix[0]) > x and len(matrix) > y
 
-def get_piece_coordinates(piece_name):
+def get_piece_coordinates(board, piece_name):
     x, y = (-1,-1) # the piece is not on the board at this point
-    for i in range(len(game_board)):
-        for k in range(len(game_board[i])):
-            if game_board[i][k] == piece_name:
+    for i in range(len(board)):
+        for k in range(len(board[i])):
+            if board[i][k] == piece_name:
                 x, y = (k, i)
     return (x,y)
 
-def is_a_piece(piece_name):
-    is_a_piece = get_piece_coordinates(piece_name) != (-1,-1)
+def is_a_piece(board, piece_name):
+    is_a_piece = get_piece_coordinates(board, piece_name) != (-1,-1)
     return is_a_piece
 
 
@@ -161,7 +180,7 @@ def enter_position(board):
         piece = input("Entrez le nom de la pièce à bouger : ")
     return (piece, pos_x, pos_y)
 
-def is_someone_on_the_path(start_pos, end_pos):
+def is_someone_on_the_path(board, start_pos, end_pos):
     result = False
     start_x, start_y = start_pos[0], start_pos[1]
     end_x, end_y = end_pos[0], end_pos[1]
@@ -169,33 +188,33 @@ def is_someone_on_the_path(start_pos, end_pos):
         for i in range((end_y - start_y)):
             if start_x < end_x:
                 if start_y < end_y:
-                    if game_board[start_y+i][start_x+i] != ' ':
+                    if board[start_y+i][start_x+i] != ' ':
                         result = True
                 else:
-                    if game_board[start_y-i][start_x+i] != ' ':
+                    if board[start_y-i][start_x+i] != ' ':
                         result = True
             else:
                 if start_y < end_y:
-                    if game_board[start_y+i][start_x-i] != ' ':
+                    if board[start_y+i][start_x-i] != ' ':
                         result = True
                 else:
-                    if game_board[start_y-i][start_x-i] != ' ':
+                    if board[start_y-i][start_x-i] != ' ':
                         result = True
     elif start_x == end_x: # on se déplace verticalement
         for i in range(abs(end_y-start_y)):
             if start_y < end_y:
-                if game_board[start_y+i][start_x] != ' ':
+                if board[start_y+i][start_x] != ' ':
                     result = True
             else:
-                if game_board[start_y-i][start_x] != ' ':
+                if board[start_y-i][start_x] != ' ':
                     result = True
     elif start_y == end_y: # on se déplace horizontalement
         for i in range(abs(end_x-start_x)):
             if start_x < end_x:
-                if game_board[start_y][start_x+i] != ' ':
+                if board[start_y][start_x+i] != ' ':
                     result = True
             else:
-                if game_board[start_y][start_x-i] != ' ':
+                if board[start_y][start_x-i] != ' ':
                     result = True  
     else:
         result = False
@@ -203,36 +222,46 @@ def is_someone_on_the_path(start_pos, end_pos):
 
 #print(is_someone_on_the_path((4,5), (0,1)))
 
-def is_position_valid(piece_name, start_pos, end_pos):
-    position_valid = True
+def is_position_valid(board, piece_name, start_pos, end_pos):
+
+    position_valid = False
     start_x, start_y = start_pos[0], start_pos[1]
     end_x, end_y = end_pos[0], end_pos[1]
-    piece_target = game_board[end_y][end_x]
+    piece_target = board[end_y][end_x]
     # PION
-    if piece_name == "p" or piece_name == "P": 
+    if piece_name in ('P', 'p'):
         if start_x == end_x:
-            if piece_target != ' ':    
+            if piece_target == ' ':    
                 distance = end_y - start_y if piece_name == "p" else start_y - end_y
-                if distance == 2 and start_y != 1:
-                    position_valid = False
+                if distance == 2 and (start_y == 1 or start_y == 6):
+                    position_valid = True
                 elif distance == 1:
                     position_valid = True
                 else:
                     position_valid = False
             else:
                 position_valid = False
+        elif end_x == start_x + 1 or end_x == start_x - 1:
+            if piece_name == "p":
+                if end_y == start_y + 1:
+                    if piece_target != ' ' and piece_target.isupper():
+                        position_valid = True
+            else:
+                if end_y == start_y - 1:
+                    if piece_target != ' ' and piece_target.islower():
+                        position_valid = True
     
     
     return position_valid
 
 
-def move_piece(piece_name, end_x, end_y):
+def move_piece(board, piece_name, end_x, end_y):
     piece_coord = get_piece_coordinates(piece_name)
     piece_target = (end_x,end_y)
     piece_moved = False
     if is_position_valid(piece_name, piece_coord, piece_target):
-        game_board[piece_coord[1]][piece_coord[0]] = ' '
-        game_board[end_y][end_x] = piece_name
+        board[piece_coord[1]][piece_coord[0]] = ' '
+        board[end_y][end_x] = piece_name
         piece_moved = True
     return piece_moved
 
@@ -244,3 +273,127 @@ def move_piece(piece_name, end_x, end_y):
 
 #print(move_piece(to_move[0], to_move[1], to_move[2]))
 #print_game_board(game_board)
+
+
+# User Interface
+scr = pygame.display.set_mode((640,640))  
+UI_board = create_and_initialise_matrix(8,8,' ') # Create a matrix for the UI game board
+
+def UI_makeboard(screen):
+    white_color = (240,217,181)
+    brown_color = (181,136,99)  
+    for k in range(8):
+        for i in range(8):
+            pygame.draw.rect(screen, white_color if (i+k) % 2 == 0 else brown_color, pygame.Rect(80*i, 80*k, 80, 80)) 
+ 
+def UI_drawpiece(board, piece_name, pos_x, pos_y):
+    img_path = "pieces/b" + piece_name + ".png" if piece_name.islower() else "pieces/w" + piece_name.lower() + ".png"
+    piece = pygame.image.load(img_path).convert_alpha()
+    piece = pygame.transform.scale(piece,(80,80))
+    board[pos_y][pos_x] = piece
+
+def UI_drawboard_from_matrice(UI_board, game_board):
+    for y in range(8):
+        for x in range(8):
+            if game_board[y][x] != ' ':
+                piece_name = game_board[y][x]
+                UI_drawpiece(UI_board, piece_name, x, y)
+
+def UI_get_legal_moves(board, piece_x, piece_y):
+    legal_moves_coord = []
+    for y in range(8):
+        for x in range(8):
+            if is_position_valid(board, board[piece_y][piece_x], (piece_x, piece_y), (x, y)):
+                legal_moves_coord += [(x, y)]
+    return legal_moves_coord
+
+
+clock = pygame.time.Clock()
+
+def UI_Init(screen, UI_board, game_board):
+    pygame.init()  
+    done = False  
+    piece_draging = False
+    last_move = False # [start_pos, end_pos]
+    while not done:  
+        UI_makeboard(screen)
+        if last_move:
+            start_x, start_y = last_move[0]
+            end_x, end_y = last_move[1]
+            yellow_color = (247,236,91)
+            hint = pygame.Surface((80,80))
+            hint.set_alpha(100)
+            hint.fill(yellow_color)
+            scr.blit(hint, [80*start_x, 80*start_y])
+            scr.blit(hint, [80*end_x, 80*end_y])
+        for event in pygame.event.get():  
+            if event.type == pygame.QUIT:  
+                done = True 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                sq_x, sq_y = (int(8*(mouse_x/640)), int(8*(mouse_y/640)))
+                
+                if game_board[sq_y][sq_x] != ' ': # Si il y a une pièce sur la case
+                    piece_draging = [sq_x, sq_y, UI_board[sq_y][sq_x], game_board[sq_y][sq_x]] # On stocke les coordonnées de la pièce, son nom et sa "surface" pour la déplacer
+                    UI_board[sq_y][sq_x] = ' ' # On enlève la pièce de l'UI
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                if piece_draging:
+                    mouse_x, mouse_y = pygame.mouse.get_pos()
+                    sq_x, sq_y = (int(8*(mouse_x/640)), int(8*(mouse_y/640)))
+                    legal_moves = UI_get_legal_moves(game_board, piece_draging[0], piece_draging[1])
+                    if (sq_x, sq_y) in legal_moves: # Si la case sélectionnée est une case valide
+                        # Play sound
+                        if UI_board[sq_y][sq_x] != ' ': # If there is a piece here
+                            pygame.mixer.music.load('audio/take.mp3')
+                        else:
+                            pygame.mixer.music.load('audio/move.mp3')
+                        pygame.mixer.music.play()
+                        
+                        # On place la pièce sur la case sélectionnée
+                        UI_board[sq_y][sq_x] = piece_draging[2] 
+                        # On met à jour le plateau de jeu (matrice)
+                        game_board[sq_y][sq_x] = piece_draging[3]
+                        game_board[piece_draging[1]][piece_draging[0]] = ' '
+                        last_move = [(piece_draging[0], piece_draging[1]), (sq_x, sq_y)]
+                    else:
+                        # On replace la pièce dans sa case d'origine
+                        UI_board[piece_draging[1]][piece_draging[0]] = piece_draging[2] 
+                    
+                    piece_draging = False # On arrête de déplacer la pièce
+        
+        if piece_draging:
+            # On affiche les cases valides
+            legal_moves = UI_get_legal_moves(game_board, sq_x, sq_y)
+            for square in legal_moves:
+                green_color = (0,255,0)
+                hint = pygame.Surface((80,80))
+                hint.set_alpha(50)
+                hint.fill(green_color)
+                scr.blit(hint, [80*square[0], 80*square[1]])
+
+        # On affiche les pièces sur le plateau
+        for y in range(8):
+            for x in range(8):
+                if UI_board[y][x] != ' ':
+                    screen.blit(UI_board[y][x],[80*x,80*y])
+
+        # Si on déplace une pièce
+        if piece_draging:
+            mouse_x, mouse_y = pygame.mouse.get_pos()  
+            # On positionne le centre de la pièce sur la souris
+            piece = piece_draging[2]
+            scr.blit(piece,[mouse_x-40,mouse_y-40])
+            
+            
+
+        
+                        
+                        
+        
+        pygame.display.flip() # Uptdate the screen
+        clock.tick(75) # 75 FPS limit
+
+
+UI_drawboard_from_matrice(UI_board, game_board)
+UI_Init(scr, UI_board, game_board)
